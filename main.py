c58386e2600b11d7d2177cd255134080312bb2d3
anis <smail.a.anis@gmail.com>
2020-02-20 19:49:12 +0100

    add submit_solution contract

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def grouped(iterable, n):
-    "s -> (s0,s1,s2,...sn-1), (sn,sn+1,sn+2,...s2n-1), (s2n,s2n+1,s2n+2,...s3n-1), ..."
     return zip(*[iter(iterable)]*n)
 
 
@@ -31,6 +30,14 @@ def parse_input_file(path):
             'libraries': list(parse_libraries())
         }
 
+
+
+def submit_solutions(d: [[]]):
+    """
+
+    :param d: [{"library_id" : 1, "nb_books": 3, "book_ids": [1, 5, 6, 4}]
+    """
+    pass
 
 def main():
     print("hello world !")
